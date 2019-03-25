66aa908711fc0bb20559fa1ef18a8e2c264ebd29
luong@thinklabs.vn <luong@thinklabs.vn>
2019-03-25 17:37:14 +0700

    #test commit

--- app/containers/LoginPage/index.js
+++ app/containers/LoginPage/index.js
@@ -27,7 +27,7 @@ class Login extends Component {
                   <CardBody>
                     <Form>
                       <h1>Login</h1>
-                      <p className="text-muted">Sign In to your account sss</p>
+                      <p className="text-muted">Sign In to your account</p>
                       <InputGroup className="mb-3">
                         <InputGroupAddon addonType="prepend">
                           <InputGroupText>
